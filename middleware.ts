975870b8c8cce81f2dd976daf1729becc4ce1f42
wave-ys <yujiasun01@outlook.com>
2024-01-19 00:35:45 +0800

    update redirect middleware

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,12 @@ import {NextResponse} from 'next/server'
 import {PATH_PREFIX} from "@/api";
 
 export function middleware(request: NextRequest) {
-  const forwardedProto = request.headers.get('x-forwarded-proto');
-  const forwardedHost = request.headers.get('x-forwarded-host');
-  if (!forwardedProto || !forwardedHost || `${forwardedProto}://${forwardedHost}` !== PATH_PREFIX)
-    return NextResponse.redirect(`${PATH_PREFIX}${request.nextUrl.pathname}${request.nextUrl.search}`)
+  if (process.env.NODE_ENV === 'development') {
+    const forwardedProto = request.headers.get('x-forwarded-proto');
+    const forwardedHost = request.headers.get('x-forwarded-host');
+    if (!forwardedProto || !forwardedHost || `${forwardedProto}://${forwardedHost}` !== PATH_PREFIX)
+      return NextResponse.redirect(`${PATH_PREFIX}${request.nextUrl.pathname}${request.nextUrl.search}`)
+  }
+
   return NextResponse.next();
 }
