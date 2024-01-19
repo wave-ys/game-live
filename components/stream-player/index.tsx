7cd0d34c1ce3b02dec845065d6a035b2970b9c5c
wave-ys <yujiasun01@outlook.com>
2024-01-19 20:34:47 +0800

    small ui fix

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -6,6 +6,7 @@ import {cn} from "@/lib/utils";
 
 import '@vidstack/react/player/styles/default/theme.css';
 import '@vidstack/react/player/styles/default/layouts/video.css';
+import './styles.css'
 
 interface StreamPlayerProps {
   className?: string;
@@ -13,10 +14,12 @@ interface StreamPlayerProps {
 
 export default function StreamPlayer({className}: StreamPlayerProps) {
   return (
-    <MediaPlayer className={cn("h-full", className)} title="Sprite Fight"
-                 src="http://localhost:8888/live/2663d813-e932-4f9d-a17f-36b88b2b6be1/index.m3u8">
-      <MediaProvider></MediaProvider>
-      <DefaultVideoLayout icons={defaultLayoutIcons}/>
-    </MediaPlayer>
+    <div className={cn("overflow-hidden h-full", className)}>
+      <MediaPlayer className={"h-full"} title="Sprite Fight"
+                   src="http://localhost:8888/live/2663d813-e932-4f9d-a17f-36b88b2b6be1/index.m3u8">
+        <MediaProvider></MediaProvider>
+        <DefaultVideoLayout icons={defaultLayoutIcons}/>
+      </MediaPlayer>
+    </div>
   )
 }
