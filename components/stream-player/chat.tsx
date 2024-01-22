375631a39d2c567edb5bdf8951f89dcbf008a2fe
wave-ys <yujiasun01@outlook.com>
2024-01-22 20:52:41 +0800

    chat ui init

--- components/stream-player/chat.tsx
+++ components/stream-player/chat.tsx
@@ -3,6 +3,7 @@ import {Button} from "@/components/ui/button";
 import {useStreamChat} from "@/store/use-stream-chat";
 import {RiExpandLeftLine, RiExpandRightLine} from "react-icons/ri";
 import {IoChatboxOutline, IoPeopleOutline} from "react-icons/io5";
+import {Input} from "@/components/ui/input";
 
 interface StreamCharProps {
   className?: string;
@@ -13,6 +14,14 @@ interface StreamChatToggleProps {
 }
 
 interface ChatVariantToggleProps {
+  className?: string;
+}
+
+interface ChatContentProps {
+  className?: string;
+}
+
+interface ChatInputBoxProps {
   className?: string;
 }
 
@@ -46,14 +55,33 @@ function ChatVariantToggle({className}: ChatVariantToggleProps) {
   )
 }
 
+function ChatContent({className}: ChatContentProps) {
+  return (
+    <div className={cn('flex items-center justify-center text-muted-foreground text-sm', className)}>
+      Welcome to the chat!
+    </div>
+  )
+}
+
+function ChatInputBox({className}: ChatInputBoxProps) {
+  return (
+    <form className={cn("flex space-x-2 p-2", className)}>
+      <Input name={"text"} placeholder={"Send a message"}/>
+      <Button type={"submit"}>Chat</Button>
+    </form>
+  )
+}
+
 export default function StreamChat({className}: StreamCharProps) {
   return (
     <div className={cn("flex flex-col", className)}>
-      <nav className={"border-b flex items-center p-2"}>
+      <nav className={"flex-none border-b flex items-center p-2"}>
         <StreamChatToggle className={"flex-none"}/>
         <span className={"font-semibold flex-auto text-center"}>Stream Chat</span>
         <ChatVariantToggle className={"flex-none"}/>
       </nav>
+      <ChatContent className={"flex-auto"}/>
+      <ChatInputBox className={"flex-none"}/>
     </div>
   )
 }
